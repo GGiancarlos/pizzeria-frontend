048e21c5ea118d61a18bab6b246b530f17bf5130
Giancarlos Gutiérrez Cervantes <ggutierrez@canvia.com>
2023-07-31 13:11:18 -0500

    feat: service created for backend test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {PizzeriaService} from "./services/pizzeria.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'pizzeria';
+
+
+  constructor(private pizzeriaService: PizzeriaService) {
+  }
+
+  ngOnInit(): void {
+    this.pizzeriaService.getPizzas().subscribe(response => {
+      console.log("PIZZAS: ", response);
+    });
+
+  }
 }
